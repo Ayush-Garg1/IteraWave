e738c0a4f707203ca0f5431831c04a25d5a0c02b
Ayush Garg <ayushgr1128@gmail.com>
2024-09-08 17:43:23 +0530

    starting with first app AUTHENTICATION

--- iterawave/asgi.py
+++ iterawave/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for IteraWave project.
+ASGI config for iterawave project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'IteraWave.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'iterawave.settings')
 
 application = get_asgi_application()
